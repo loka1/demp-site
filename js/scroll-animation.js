01aa46918e8124aeb255c2b5444b06fa7d3111d1
lokas <lokashafeek7755@gmail.com>
2025-04-19 16:06:24 +0200

    fixing

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -7,33 +7,33 @@ aboutVideo.addEventListener('click', function() {
     this.load();
   }
 });
-document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll('.animate-on-scroll');
+// document.addEventListener('DOMContentLoaded', function() {
+//     const sections = document.querySelectorAll('.animate-on-scroll');
 
-    function checkScreenSize() {
-        if (window.innerWidth < 992) {
-            document.body.classList.add('mobile-device');
-        } else {
-            document.body.classList.remove('mobile-device');
-        }
-    }
+//     function checkScreenSize() {
+//         if (window.innerWidth < 992) {
+//             document.body.classList.add('mobile-device');
+//         } else {
+//             document.body.classList.remove('mobile-device');
+//         }
+//     }
 
-    function checkScroll() {
-        sections.forEach(section => {
-            const sectionTop = section.offsetTop;
-            const sectionHeight = section.offsetHeight;
-            const windowBottom = window.scrollY + window.innerHeight;
+//     function checkScroll() {
+//         sections.forEach(section => {
+//             const sectionTop = section.offsetTop;
+//             const sectionHeight = section.offsetHeight;
+//             const windowBottom = window.scrollY + window.innerHeight;
 
-            if (windowBottom > sectionTop + sectionHeight / 4) {
-                const animationClass = section.dataset.animation || 'animate__fadeIn';
-                if (window.innerWidth >= 992) {
-                    section.classList.add('animate__animated', animationClass);
-                }
-                section.classList.remove('animate-on-scroll');
-            } 
-        });
-    }
+//             if (windowBottom > sectionTop + sectionHeight / 4) {
+//                 const animationClass = section.dataset.animation || 'animate__fadeIn';
+//                 if (window.innerWidth >= 992) {
+//                     section.classList.add('animate__animated', animationClass);
+//                 }
+//                 section.classList.remove('animate-on-scroll');
+//             } 
+//         });
+//     }
 
-    window.addEventListener('scroll', checkScroll);
-    checkScroll(); // Initial check on page load
-});
+//     window.addEventListener('scroll', checkScroll);
+//     checkScroll(); // Initial check on page load
+// });
